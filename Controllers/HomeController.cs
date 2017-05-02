4ba04644740560ce26d9af0ed379ff25b116209b
Rian Finnegan <flanagan89@gmail.com>
2017-05-02 11:00:44 +1000

    basic home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Rian.Cognitive;
 
 namespace Sentiment_And_KeyPhrases.Controllers
 {
     public class HomeController : Controller
     {
+        private Task _startDownloading;
+        private Manager _manager;
+        public HomeController()
+        {
+             _manager = new Manager();
+             _startDownloading = _manager.DownloadLastTopicDetection();
+             
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,13 +25,6 @@ namespace Sentiment_And_KeyPhrases.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
